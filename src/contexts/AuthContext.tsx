7c14009085e15d9c613fcdd47526ed307b63149b
Kauã Steliano <stelianok@gmail.com>
2020-11-30 16:51:58 -0300

    Created useAuth Hook

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,30 +1,66 @@
-import React, { createContext, useCallback } from 'react';
+import { AnyMxRecord } from 'dns';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 import api from '../services/api';
 
+interface AuthState {
+  token: string;
+  user: any;
+}
 interface SignInCredentials {
   email: string;
   password: string;
 }
 interface AuthContextData {
-  name: string;
-  signIn(credentials: SignInCredentials): void;
+  user: any;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 export const AuthContext = createContext<AuthContextData>(
   {} as AuthContextData,
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('/sessions', {
       email,
       password,
     });
+    const { token, user } = response.data;
 
-    console.log(response.data);
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
   }, []);
   return (
-    <AuthContext.Provider value={{ name: 'Kaua', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
@@ -6,7 +6,7 @@ import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { AuthContext } from '../../contexts/AuthContext';
+import { useAuth } from '../../contexts/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Background, Content } from './styles';
 
@@ -16,7 +16,7 @@ interface SignInFormData {
 }
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const { signIn } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
